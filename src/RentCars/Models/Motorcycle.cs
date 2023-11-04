5552dc77e3ea038dbe2ef29c6f1b2ba7aca983c5
Danilo <dansantosalfa@gmail.com>
2023-11-04 12:28:50 -0300

    feat: Req 7 feito.

--- src/RentCars/Models/Motorcycle.cs
+++ src/RentCars/Models/Motorcycle.cs
@@ -5,4 +5,7 @@ namespace RentCars.Models;
 //7 - Faça a classe `Motorcycle` herdar de `Vehicle`
 public class Motorcycle : Vehicle
 {
+    public double SeatHeight { get; set; }
+    public BrakeType FrontBrake { get; set; }
+    public BrakeType RearBrake { get; set; }
 }
