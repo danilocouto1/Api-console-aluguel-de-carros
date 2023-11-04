10969e5d7d4590ed4247a075215ea2b5abb2e6f4
Danilo <dansantosalfa@gmail.com>
2023-11-04 12:30:03 -0300

    feat: Req 8 feito.

--- src/RentCars/Models/PickupTruck.cs
+++ src/RentCars/Models/PickupTruck.cs
@@ -4,5 +4,5 @@ namespace RentCars.Models;
 //8 - Faça a classe `PickupTruck` herdar de `Car`
 public class PickupTruck : Car
 {
-    public double LoadCapacity;
+    public double LoadCapacity {  get; set; }
 }
