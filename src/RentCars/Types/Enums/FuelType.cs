6b17feb3edd8511e29eae2583e3fc6d7cf6bd0c2
Danilo <dansantosalfa@gmail.com>
2023-11-04 12:15:18 -0300

    feat: Req 2 feito.

--- src/RentCars/Types/Enums/FuelType.cs
+++ src/RentCars/Types/Enums/FuelType.cs
@@ -3,4 +3,10 @@ namespace RentCars.Types;
 //2 - Crie um enum `FuelType`
 public enum FuelType
 {
+    Alcohol = 10,
+    Gasoline = 20,
+    Flex = 30,
+    Diesel = 40,
+    Electric = 50,
+    Hybrid = 60
 }
