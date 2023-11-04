356108736f1b5d0f89830e7800bfd4d9426f1215
Danilo <dansantosalfa@gmail.com>
2023-11-04 12:24:20 -0300

    feat: Req 3 feito.

--- src/RentCars/Models/Vehicle.cs
+++ src/RentCars/Models/Vehicle.cs
@@ -4,4 +4,14 @@ namespace RentCars.Models;
 //3 - Crie a classe `Vehicle`
 public class Vehicle
 {
+    public string Brand { get; set; } = "";
+    public string Model { get; set; } = "";
+    public decimal Price { get; set; }
+    public FuelType Fuel { get; set; }
+    public int EngineCapacity { get; set; }
+    public Color MainColor { get; set; }
+    public int Year { get; set; }
+    public double PricePerDay { get; set; }
+    public bool IsRented { get; set; } = false;
+
 }
