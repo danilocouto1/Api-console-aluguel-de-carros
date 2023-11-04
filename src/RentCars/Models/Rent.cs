ded7755ff4fccbd3cd93f4fb4b5d8455c5f4ab3a
Danilo <dansantosalfa@gmail.com>
2023-11-04 12:51:24 -0300

    feat: Req Bonus feito.

--- src/RentCars/Models/Rent.cs
+++ src/RentCars/Models/Rent.cs
@@ -30,12 +30,12 @@ public class Rent
     //11 - Implemente os métodos de `cancelar` e `finalizar` um aluguel
     public void Cancel()
     {
-        throw new NotImplementedException();
+        Status = RentStatus.Canceled;
     }
 
     //11 - Implemente os métodos de `cancelar` e `finalizar` um aluguel
     public void Finish()
     {
-        throw new NotImplementedException();
+        Status = RentStatus.Finished;
     }
 }
