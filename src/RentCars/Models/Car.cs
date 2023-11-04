5ed7350cee7d14bdd9bf93fa1ebfbbfbadbd8ac9
Danilo <dansantosalfa@gmail.com>
2023-11-04 12:27:20 -0300

    feat: Req 6 feito.

--- src/RentCars/Models/Car.cs
+++ src/RentCars/Models/Car.cs
@@ -2,6 +2,12 @@ using RentCars.Types;
 namespace RentCars.Models;
 
 //6 - Faça a classe `Car` herdar de `Vehicle`
-public class Car
+public class Car : Vehicle
 {
+    public int Seats { get; set; }
+    public int Doors { get; set; }
+    public TractionType Traction { get; set; }
+    public BrakeType FrontBrake { get; set; }
+    public BrakeType RearBrake { get; set; }
+
 }
